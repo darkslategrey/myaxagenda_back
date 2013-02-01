00ccb7fc63cc80f51ea75fd071f10419a16fd0bd
grego <greg.faruch@gmail.com>
2013-02-01 01:02:22 +0100

    add sql schema + s/regie/bannieres

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,22 +4,23 @@ require 'logger'
 require './models/calendar.rb'
 require './models/event_types.rb'
 
-Calendar.find_by_code('REGIE_JOBENFANCE') || Calendar.create(:name => 'Régie Jobenfance',
-                                                             :code => 'REGIE_JOBENFANCE',
-                                                             :description => 'Régie Jobenfance',
-                                                             :color => '')
-Calendar.find_by_code('REGIE_JOBDEPENDANCE') || Calendar.create(:name => 'Régie Jobdependance',
-                                                                :code => 'REGIE_JOBDEPENDANCE',
-                                                                :description => 'Régie Jobdependance',
-                                                                :color => '')
 Calendar.find_by_code('ACTIONS_JOBENFANCE') || Calendar.create(:name => 'Actions Jobenfance',
                                                                :code => 'ACTIONS_JOBENFANCE',
-                                                               :description => 'Régie Jobenfance',
+                                                               :description => 'Actions Jobenfance',
                                                                :color => '')
 Calendar.find_by_code('ACTIONS_JOBDEPENDANCE') || Calendar.create(:name => 'Actions Jobdependance',
                                                                   :code => 'ACTIONS_JOBDEPENDANCE',
-                                                                  :description => 'Régie Jobdependance',
+                                                                  :description => 'Actions Jobdependance',
                                                                   :color => '')
+Calendar.find_by_code('REGIE_JOBENFANCE') || Calendar.create(:name => 'Bannières Jobenfance',
+                                                             :code => 'REGIE_JOBENFANCE',
+                                                             :description => 'Bannières Jobenfance',
+                                                             :color => '')
+Calendar.find_by_code('REGIE_JOBDEPENDANCE') || Calendar.create(:name => 'Bannière Jobdependance',
+                                                                :code => 'REGIE_JOBDEPENDANCE',
+                                                                :description => 'Bannière Jobdependance',
+                                                                :color => '')
+
 
 
 
@@ -30,14 +31,14 @@ params = {
   :id => je_id,
   :code => 'AC_REGIE',
   :type => 'user',
-  :libelle => 'Régie Jobenfance',
+  :libelle => 'Bannières Jobenfance',
   :active => 1,
   :position => 30
 }
 
 EventTypeJe.find_by_code('AC_REGIE') || EventTypeJe.create(params)
 params[:id] = jd_id
-params[:libelle] = 'Régie Jobdependance'
+params[:libelle] = 'Bannières Jobdependance'
 EventTypeJd.find_by_code('AC_REGIE') || EventTypeJd.create(params)
 
 
